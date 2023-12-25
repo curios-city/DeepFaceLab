4894de6666cb3d83e83d1da7dc91105d72dffaff
curios-city <115023083+curios-city@users.noreply.github.com>
2023-12-26 03:56:28 +0800

    Update XSegUtil.py

--- mainscripts/XSegUtil.py
+++ mainscripts/XSegUtil.py
@@ -17,15 +17,15 @@ import pickle
 
 def is_packed(input_path):
     if PackedFaceset.path_contains(input_path):
-        io.log_info (f'\n{input_path} contains packed faceset! Unpack it first.\n')
+        io.log_info (f'\n{input_path} 包含打包的人脸集！请先解压\n')
         return True
 
 def apply_xseg(input_path, model_path):
     if not input_path.exists():
-        raise ValueError(f'{input_path} not found. Please ensure it exists.')
+        raise ValueError(f'{input_path} 未找到，请确保它存在.')
 
     if not model_path.exists():
-        raise ValueError(f'{model_path} not found. Please ensure it exists.')
+        raise ValueError(f'{model_path} 未找到，请确保它存在.')
 
     if is_packed(input_path) : return
         
@@ -41,7 +41,7 @@ def apply_xseg(input_path, model_path):
         
         
     if face_type is None:
-        face_type = io.input_str ("XSeg model face type", 'same', ['h','mf','f','wf','head','same', 'custom'], help_message="Specify face type of trained XSeg model. For example if XSeg model trained as WF, but faceset is HEAD, specify WF to apply xseg only on WF part of HEAD. Default is 'same'").lower()
+        face_type = io.input_str ("XSeg模型人脸类型 XSeg model face type", 'same', ['h','mf','f','wf','head','same', 'custom'], help_message="指定经过训练的XSeg模型的面部类型。例如，如果XSeg模型训练为WF，但facesset是HEAD，则指定WF仅对HEAD的WF部分应用XSeg。默认值 'same'").lower()
         if face_type == 'same':
             face_type = None
     
@@ -53,7 +53,7 @@ def apply_xseg(input_path, model_path):
                      'custom' : FaceType.CUSTOM,
                      'head' : FaceType.HEAD}[face_type]
                      
-    io.log_info(f'Applying trained XSeg model to {input_path.name}/ folder.')
+    io.log_info(f'将训练好的 XSeg 模型应用于 {input_path.name}/ 文件夹.')
 
     device_config = nn.DeviceConfig.ask_choose_device(choose_only_one=True)
     nn.initialize(device_config)
@@ -72,7 +72,7 @@ def apply_xseg(input_path, model_path):
     for filepath in io.progress_bar_generator(images_paths, "Processing"):
         dflimg = DFLIMG.load(filepath)
         if dflimg is None or not dflimg.has_data():
-            io.log_info(f'{filepath} is not a DFLIMG')
+            io.log_info(f'{filepath} 不是DFLIMG文件')
             continue
         
         img = cv2_imread(filepath).astype(np.float32) / 255.0
@@ -112,14 +112,14 @@ def apply_xseg(input_path, model_path):
         
 def fetch_xseg(input_path):
     if not input_path.exists():
-        raise ValueError(f'{input_path} not found. Please ensure it exists.')
+        raise ValueError(f'{input_path} 未找到，请确保它存在')
 
     if is_packed(input_path) : return
     
     output_path = input_path.parent / (input_path.name + '_xseg')
     output_path.mkdir(exist_ok=True, parents=True)
     
-    io.log_info(f'Copying faces containing XSeg polygons to {output_path.name}/ folder.')
+    io.log_info(f'将包含Xseg遮罩的人脸图片复制到 {output_path.name}/ 文件夹')
     
     images_paths = pathex.get_image_paths(input_path, return_Path_class=True)
     
@@ -128,7 +128,7 @@ def fetch_xseg(input_path):
     for filepath in io.progress_bar_generator(images_paths, "Processing"):
         dflimg = DFLIMG.load(filepath)
         if dflimg is None or not dflimg.has_data():
-            io.log_info(f'{filepath} is not a DFLIMG')
+            io.log_info(f'{filepath} 不是 DFLIMG文件')
             continue
         
         ie_polys = dflimg.get_seg_ie_polys()
@@ -137,24 +137,22 @@ def fetch_xseg(input_path):
             files_copied.append(filepath)
             shutil.copy ( str(filepath), str(output_path / filepath.name) )
     
-    io.log_info(f'Files copied: {len(files_copied)}')
+    io.log_info(f'已复制文件数: {len(files_copied)}')
     
-    is_delete = io.input_bool (f"\r\nDelete original files?", True)
+    is_delete = io.input_bool (f"\r\n删除原始文件?", True)
     if is_delete:
         for filepath in files_copied:
             Path(filepath).unlink()
                
 def remove_xseg(input_path):
     if not input_path.exists():
-        raise ValueError(f'{input_path} not found. Please ensure it exists.')
+        raise ValueError(f'{input_path} 未找到，请确保它存在')
 
     if is_packed(input_path) : return
     
-    io.log_info(f'Processing folder {input_path}')
-    io.log_info('!!! WARNING : APPLIED XSEG MASKS WILL BE REMOVED FROM THE FRAMES !!!')
-    io.log_info('!!! WARNING : APPLIED XSEG MASKS WILL BE REMOVED FROM THE FRAMES !!!')
-    io.log_info('!!! WARNING : APPLIED XSEG MASKS WILL BE REMOVED FROM THE FRAMES !!!')
-    io.input_str('Press enter to continue.')
+    io.log_info(f'处理文件夹 {input_path}')
+
+    io.input_str('按回车enter键继续')
                                
     images_paths = pathex.get_image_paths(input_path, return_Path_class=True)
     
@@ -162,25 +160,23 @@ def remove_xseg(input_path):
     for filepath in io.progress_bar_generator(images_paths, "Processing"):
         dflimg = DFLIMG.load(filepath)
         if dflimg is None or not dflimg.has_data():
-            io.log_info(f'{filepath} is not a DFLIMG')
+            io.log_info(f'{filepath} 不是 DFLIMG文件')
             continue
         
         if dflimg.has_xseg_mask():
             dflimg.set_xseg_mask(None)
             dflimg.save()
             files_processed += 1
-    io.log_info(f'Files processed: {files_processed}')
+    io.log_info(f'已处理文件数: {files_processed}')
     
 def remove_xseg_labels(input_path):
     if not input_path.exists():
-        raise ValueError(f'{input_path} not found. Please ensure it exists.')
+        raise ValueError(f'{input_path} 未找到，请确保它存在')
 
     if is_packed(input_path) : return
     
-    io.log_info(f'Processing folder {input_path}')
-    io.log_info('!!! WARNING : LABELED XSEG POLYGONS WILL BE REMOVED FROM THE FRAMES !!!')
-    io.log_info('!!! WARNING : LABELED XSEG POLYGONS WILL BE REMOVED FROM THE FRAMES !!!')
-    io.log_info('!!! WARNING : LABELED XSEG POLYGONS WILL BE REMOVED FROM THE FRAMES !!!')
+    io.log_info(f'处理文件夹 {input_path}')
+
     io.input_str('Press enter to continue.')
     
     images_paths = pathex.get_image_paths(input_path, return_Path_class=True)
@@ -189,7 +185,7 @@ def remove_xseg_labels(input_path):
     for filepath in io.progress_bar_generator(images_paths, "Processing"):
         dflimg = DFLIMG.load(filepath)
         if dflimg is None or not dflimg.has_data():
-            io.log_info(f'{filepath} is not a DFLIMG')
+            io.log_info(f'{filepath} 不是 DFLIMG 文件')
             continue
 
         if dflimg.has_seg_ie_polys():
@@ -197,4 +193,4 @@ def remove_xseg_labels(input_path):
             dflimg.save()            
             files_processed += 1
             
-    io.log_info(f'Files processed: {files_processed}')
+    io.log_info(f'已处理文件数: {files_processed}')
